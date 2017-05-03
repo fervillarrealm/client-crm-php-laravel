b9284b75e0ed0feb7c435ea5842f976fe987b3dc
Fernando Villarreal <fer.villarreal@gmail.com>
2017-05-03 20:42:45 +0000

    Gulp & Bower Vendors

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function(){
+    
+    Route::get('/login', [
+        'uses'  => 'LoginController@getLogin',
+        'as'    => 'login'
+    ]);
+    
+    Route::get('/test', function (){
+        return view('test');
+    });
+    
+    Route::group(['middleware' => 'auth'], function() {
+        
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,91 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minify = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var phpunit = require('gulp-phpunit');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var paths = {  
+    'dev': {
+        'less': './resources/assets/less/',
+        'js': './resources/assets/js/',
+        'vendor': './resources/assets/vendor/'
+    },
+    'production': {
+        'css': './public/assets/css/',
+        'js': './public/assets/js/'
+    }
+};
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+
+//-------------------- TASKS -----------------------//
+//  LESS
+gulp.task('less', function() {  
+  return gulp.src(paths.dev.less+'app.less')
+    .pipe(less())
+    .pipe(gulp.dest(paths.production.css))
+    .pipe(minify({keepSpecialComments:0}))
+    //.pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(paths.production.css));
 });
+
+//  CSS
+gulp.task('css', function (){
+  return gulp.src([
+    paths.dev.vendor + 'datatables/media/css/jquery.dataTables.css',
+    paths.dev.vendor + 'datatables/media/css/dataTables.bootstrap.css',
+    paths.dev.vendor + 'font-awesome/css/font-awesome.css',
+    paths.dev.vendor + 'sweetalert2/dist/sweetalert2.css',
+    paths.dev.vendor + 'AdminLTE/dist/css/AdminLTE.css',
+    paths.dev.vendor + 'AdminLTE/dist/css/skins/skin-blue.css',
+    ])
+    .pipe(concat('main.css'))
+    .pipe(minify({keppSpecialComments:0}))
+    //.pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(paths.production.css));
+});
+
+//  JS
+gulp.task('js', function(){  
+  return gulp.src([
+      paths.dev.vendor+'jquery/dist/jquery.js',
+      paths.dev.vendor+'bootstrap/dist/js/bootstrap.js',
+      paths.dev.vendor+'datatables/media/js/jquery.dataTables.js',
+      paths.dev.vendor+'datatables/media/js/dataTables.bootstrap.js',
+      paths.dev.vendor+'sweetalert2/dist/sweetalert2.js',
+      paths.dev.vendor+'AdminLTE/dist/js/app.js',
+      paths.dev.js+'js'
+    ])
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.production.js));
+});
+
+//  PHP Unit
+gulp.task('phpunit', function() {  
+  var options = {debug: false, notify: true};
+  return gulp.src('./tests/*.php')
+    .pipe(phpunit('./vendor/bin/phpunit', options))
+
+    .on('error', notify.onError({
+      title: 'PHPUnit Failed',
+      message: 'One or more tests failed.'
+    }))
+
+    .pipe(notify({
+      title: 'PHPUnit Passed',
+      message: 'All tests passed!'
+    }));
+});
+
+//  WATCH
+gulp.task('watch', function() {  
+  gulp.watch(paths.dev.less + '/*.less', ['css']);
+  gulp.watch(paths.dev.js + '/*.js', ['js']);
+  gulp.watch(paths.dev.js + '/*.css', ['css']);
+  gulp.watch('./tests/*.php', ['phpunit']);
+});
+
+gulp.task('default', ['less', 'css', 'js', 'phpunit', 'watch']);  
